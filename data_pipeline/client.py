a2dc19bf24d18eea0502a481025bfcbfac1bcbb9
Pujun Bhatnagar <pujun@yelp.com>
2015-07-08 17:04:59 -0700

    refactoring client.py'

--- data_pipeline/client.py
+++ data_pipeline/client.py
@@ -26,11 +26,11 @@ class Client(object):
         self._id = client_id
 
     def __init__(self, client_type, client_id='007'):
-        self.monitoring_message = _MonitoringMessage(client_type, client_id)
+        self.monitoring_message = _Monitor(client_type, client_id)
         self.id = client_id
 
 
-class _MonitoringMessage(object):
+class _Monitor(object):
     """The class that implements functionality of monitoring the messages published/
     consumed by clients using kafka.
 
@@ -38,38 +38,11 @@ class _MonitoringMessage(object):
         client_type: type of the client the monitoring_message is associated to.
             could be either producer or consumer
         client_id: id of the associated client
-        window_width: the duration for which monitoring_message will monitor
+        window_width: the duration (in second) for which monitoring_message
+             will monitor
         start_time: start_time when monitoring_message for client will start
             counting the number of messages produced/consumed
     """
-
-    @property
-    def window_width(self):
-        """The duration for which monitoring_message will monitor (i.e count
-        the number of messages) published to kafka
-
-        TODO: This is set to a random value. Have to discuss this with team
-        and finalize a value
-        """
-        return self._window_width
-
-    @window_width.setter
-    def window_width(self, window_width):
-        self._window_width = window_width
-
-    @property
-    def start_time(self):
-        """Start time when monitoring_message for client will start
-        monitoring the number of messages produced/consumed
-
-        TODO: Have to discuss with the team to finalize a value. One option
-        could be to set it to the UNIX time when the project is deployed
-        """
-        return self._start_time
-
-    @start_time.setter
-    def start_time(self, start_time):
-        self._start_time = start_time
 
     def message(self, topic):
         """Message containing monitoring information about the number
@@ -93,6 +66,17 @@ class _MonitoringMessage(object):
     def publisher(self, publisher):
         self._publisher = publisher
 
+    def get_default_record(self, topic):
+        self.record[topic] = {
+            'topic': topic,
+            'client_type': self.client_type,
+            'message_count': 0,
+            'start_timestamp': self.start_time,
+            'host_info': socket.gethostname(),
+            'client_id': self.client_id
+        }
+        return self.record[topic]
+
     def get_record(self, topic):
         """returns the record associated with the topic for any client.
         If the topic has no record, a new record is created and returned.
@@ -100,42 +84,36 @@ class _MonitoringMessage(object):
         if topic in self.record:
             return self.record[topic]
         else:
-            print "record for topic ", topic, " being created"
-            self.record[topic] = dict(
-                topic=topic,
-                client_type=self.client_type,
-                message_count=0,
-                start_timestamp=self.start_time,
-                host_info=socket.gethostname(),
-                client_id=self.client_id
-            )
-            return self.record[topic]
+            return self.get_default_record(topic)
 
     def publish(self, topic):
         """Publishing the results stored in the monitoring_message using
         the monitoring_publisher
         """
-        self.publisher.publish(self.message(topic))
+        self.producer.publish(self.message(topic))
 
-    def reset_record(self, topic, updated_start_timestamp):
+    def reset_record(self, topic, timestamp):
         """resets the record for a particular topic by resetting
         message_count to 1 (since the program has already published one
         message) and updating the start_timestamp to the start_timestamp
         of the newly published message
         """
-        self.record[topic]['message_count'] = 1
-        self.record[topic]['start_timestamp'] = updated_start_timestamp
+        self.get_default_record(topic)
+        self.get_record(topic)['start_timestamp'] = self.get_updated_start_timestamp(timestamp)
+
+    def get_updated_start_timestamp(self, timestamp):
+        return (timestamp / self.monitoring_width_in_sec) * self.monitoring_width_in_sec
 
     def get_message_count(self, topic):
         """Number of messages produced/consumed by the client
         """
-        return self.record[topic]['message_count']
+        return self.get_record(topic)['message_count']
 
     def increment_message_count(self, topic):
         """Increments the message_count in the record for the
         topic.
         """
-        self.record[topic]['message_count'] += 1
+        self.get_record(topic)['message_count'] += 1
 
     def _notify_messages_published(self, position_data):
         """Called to notify the client of successfully published messages.
@@ -149,16 +127,15 @@ class _MonitoringMessage(object):
         self.position_data = position_data
 
     def monitor(self, message):
-        if self.get_record(message.topic)['start_timestamp'] + self.window_width < message.timestamp:
+        if self.get_record(message.topic)['start_timestamp'] + self.monitoring_width_in_sec < message.timestamp:
             self.publish(message.topic)
             self.reset_record(message.topic, message.timestamp)
-        else:
-            self.increment_message_count(message.topic)
+        self.increment_message_count(message.topic)
 
-    def __init__(self, client_type, client_id, window_width=1000, start_time=0):
-        self.publisher = LoggingKafkaProducer(self._notify_messages_published)
+    def __init__(self, client_type, client_id, monitoring_width_in_sec=1000, start_time=0):
+        self.producer = LoggingKafkaProducer(self._notify_messages_published)
         self.record = {}
         self.client_type = client_type
         self.client_id = client_id
-        self.window_width = window_width
+        self.monitoring_width_in_sec = monitoring_width_in_sec
         self.start_time = start_time
