28cf309e290ce6b9e422f92fe1fbf5cff6a2fc2d
Amit Katti <askatti@yelp.com>
2015-09-09 17:41:22 -0700

    Fixed envelope tests

--- tests/envelope_test.py
+++ tests/envelope_test.py
@@ -10,13 +10,22 @@ from data_pipeline import message as dp_message
 class TestEnvelope(object):
 
     @pytest.fixture(params=[
+        {'transaction_id': None},
+        {'transaction_id': 'cluster_name:log_file_name:log_position'}
+    ])
+    def transaction_id_params(self, request):
+        return request.param
+
+    @pytest.fixture(params=[
         (dp_message.CreateMessage, {}),
         (dp_message.RefreshMessage, {}),
         (dp_message.DeleteMessage, {}),
         (dp_message.UpdateMessage, {'previous_payload': bytes(20)})
     ])
-    def message(self, request, topic_name, payload):
+    def message(self, request, topic_name, payload, transaction_id_params):
         message_class, additional_params = request.param
+        if transaction_id_params:
+            additional_params.update(transaction_id_params)
         return message_class(
             topic_name,
             10,
@@ -25,15 +34,15 @@ class TestEnvelope(object):
         )
 
     @pytest.fixture
-    def expected_message(self, message):
+    def expected_message(self, message, transaction_id_params):
         previous_payload = None
         if isinstance(message, dp_message.UpdateMessage):
             previous_payload = message.previous_payload
-
         return dict(
             encryption_type=None,
             message_type=message.message_type.name,
             meta=None,
+            transaction_id=transaction_id_params['transaction_id'],
             payload=message.payload,
             previous_payload=previous_payload,
             schema_id=message.schema_id,
