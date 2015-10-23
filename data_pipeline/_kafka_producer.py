5ff80c43ecafa8f1f7019f59595b8035f42ccf35
Matthew Krane <krane@yelp.com>
2015-10-23 16:12:35 -0700

    updated location of key

--- data_pipeline/_kafka_producer.py
+++ data_pipeline/_kafka_producer.py
@@ -63,7 +63,7 @@ class KafkaProducer(object):
         self.skip_messages_with_pii = get_config().skip_messages_with_pii
         self.user = get_config().user
         self.acceptable_users = ['batch']
-        self.key_location = 'srv-configs/ecosystem/devc/data_pipeline/key-{}.key'
+        self.key_location = '/nail/srv/configs/ecosystem/devc/data_pipeline/key-{}.key'
 
     def wake(self):
         """Should be called periodically if we're not otherwise waking up by
