e72a6c53574abdb99ecbded8393ef6993a4b2419
Justin Cunningham <justinc@yelp.com>
2015-11-10 18:05:35 -0800

    Added tailer debugging features

--- data_pipeline/tools/tailer.py
+++ data_pipeline/tools/tailer.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+import re
 import signal
 from optparse import OptionGroup
 from uuid import UUID
@@ -11,7 +12,9 @@ from yelp_batch.batch import batch_command_line_options
 from yelp_batch.batch import batch_configure
 from yelp_servlib.config_util import load_default_config
 
+import data_pipeline
 from data_pipeline._fast_uuid import FastUUID
+from data_pipeline.base_consumer import ConsumerTopicState
 from data_pipeline.config import get_config
 from data_pipeline.consumer import Consumer
 from data_pipeline.expected_frequency import ExpectedFrequency
@@ -20,6 +23,13 @@ from data_pipeline.schematizer_clientlib.schematizer import get_schematizer
 
 class Tailer(Batch):
     enable_error_emails = False
+
+    @property
+    def version(self):
+        """Overriding this so we'll get the clientlib version number when
+        the tailer is run with --version.
+        """
+        return "data_pipeline {}".format(data_pipeline.__version__)
 
     @batch_command_line_options
     def _define_tailer_options(self, option_parser):
@@ -36,12 +46,12 @@ class Tailer(Batch):
             default=[],
             help=(
                 'The topic to tail.  Can be specified multiple times to tail '
-                'more than one topic.'
+                'more than one topic.  Topics can optionally include an offset '
+                'by formatting the topic like "topic_name|offset".'
             ),
         )
         opt_group.add_option(
             '--config-file',
-            action='store_true',
             help=(
                 'If set, will use the provided configuration file to setup '
                 'the data pipeline tools. '
@@ -51,7 +61,6 @@ class Tailer(Batch):
         )
         opt_group.add_option(
             '--env-config-file',
-            action='store_true',
             help=(
                 'If set, will use the provided configuration file as the env '
                 'overrides file to setup the data pipeline tools. '
@@ -88,6 +97,24 @@ class Tailer(Batch):
                 '(Default is %default)'
             )
         )
+        opt_group.add_option(
+            '--message-limit',
+            type='int',
+            default=None,
+            help=(
+                'Provide a message limit to quit when the specified number of '
+                'messages have been output.'
+            )
+        )
+        opt_group.add_option(
+            '--include-envelope-data',
+            default=False,
+            action='store_true',
+            help=(
+                'If set, envelope info will be output along with the message '
+                'contents.'
+            )
+        )
 
         return opt_group
 
@@ -98,18 +125,33 @@ class Tailer(Batch):
             self.options.env_config_file
         )
 
+        self._setup_topics()
+
         if self.options.namespace or self.options.source:
             schematizer = get_schematizer()
             additional_topics = schematizer.get_topics_by_criteria(
                 namespace_name=self.options.namespace,
                 source_name=self.options.source
             )
-            self.options.topics.extend(
-                [topic.name for topic in additional_topics]
-            )
+            for topic in additional_topics:
+                if str(topic.name) not in self.topics:
+                    self.topics[str(topic.name)] = None
 
-        if len(self.options.topics) == 0:
+        if len(self.topics) == 0:
             self.option_parser.error("At least one topic must be specified.")
+
+    def _setup_topics(self):
+        self.topics = {}
+        for topic in self.options.topics:
+            offset = None
+
+            # https://regex101.com/r/fL0eD9/3
+            match = re.match('^(.*)\|(\d+)$', topic)
+            if match:
+                topic = match.group(1)
+                offset = ConsumerTopicState({0: int(match.group(2))}, None)
+
+            self.topics[str(topic)] = offset
 
     @batch_configure
     def _configure_signals(self):
@@ -133,19 +175,27 @@ class Tailer(Batch):
             ),
             'bam',
             ExpectedFrequency.constantly,
-            {str(topic): None for topic in self.options.topics},
+            self.topics,
             auto_offset_reset=self.options.offset_reset_location
         ) as consumer:
-            while self.running:
+            message_count = 0
+            while self.keep_running(message_count):
                 message = consumer.get_message(blocking=True, timeout=0.1)
                 if message is not None:
-                    # TODO(justinc|DATAPIPE-522): We should allow additional
-                    # output options.
-                    print message.payload_data
+                    print self._format_message(message)
+                    message_count += 1
 
-    @property
-    def running(self):
-        return self._running
+    def _format_message(self, message):
+        if self.options.include_envelope_data:
+            return message
+
+        return message.payload_data
+
+    def keep_running(self, message_count):
+        return self._running and (
+            self.options.message_limit is None or
+            message_count < self.options.message_limit
+        )
 
 
 if __name__ == '__main__':
