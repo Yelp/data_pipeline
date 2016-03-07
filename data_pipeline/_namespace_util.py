8c438f676a4080d664a3b32ee628f96e5c1bad4f
Keegan Parker <keegan@yelp.com>
2016-03-07 15:34:32 -0800

    Added optional restrictions to namespace construction from name, and general namespace name construction from components

--- data_pipeline/_namespace_util.py
+++ data_pipeline/_namespace_util.py
@@ -1,25 +1,135 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
 import re
 
-class NamespaceInfo(object):
-    """Class that provides namespace information based on a given namespace name.
 
-    Currently we treat namespaces under format detailed in FORMAT ((main|dev)\.)?cluster\.database(\.transformer)*
-    Note: we use regex for validation but not processing
+class Namespace(object):
+    """Class that provides namespace information based on a given namespace name for database-source namespaces
+    (i.e. ones created/used by replication handler).
     """
     ENVIRONMENTS = ['main', 'dev']
-    FORMAT = re.compile(r"^((main|dev)\.)?([a-zA-Z0-9_-]+)\.([a-zA-Z0-9_-]+)(\.[a-zA-Z0-9_-]+)*$")
 
-    def __init__(self, namespace_name):
-        if not self.FORMAT.match(namespace_name):
-            raise ValueError("Namespace name not in proper format for info to be extracted")
+    def __init__(
+        self,
+        namespace_name=None,
+        environment=None,
+        cluster=None,
+        database=None,
+        transformers=[]
+    ):
+        """Creates NamespaceInfo object, if given a namespace_info, will use any other given information
+        (if present) to create the namespace info object.
+
+        Params:
+        environment (optional(str)): Guaranteed to make this the environment if namespace_name makes it possible
+        cluster (optional(str)): Guaranteed to make this the cluster if namespace_name makes it possible
+        database (optional(str)): Guaranteed to make this the database if namespace_name makes it possible
+        transformers (optional(listof(str))): Guaranteed that the transformers
+                                              will include everything in transformers if namespace_name
+                                              makes it possible."""
+        if namespace_name is None and (
+            cluster is None or
+            database is None
+        ):
+            raise ValueError("namespace_name or all of cluster, database must be defined")
+        if namespace_name:
+            self._create_from_namespace_name(
+                namespace_name,
+                environment=environment,
+                cluster=cluster,
+                database=database,
+                transformers=transformers
+            )
+
+            self._validate_optional_params_satisfied(
+                environment=environment,
+                cluster=cluster,
+                database=database,
+                transformers=transformers
+            )
+        else:
+            self.environment = environment
+            self.cluster = cluster
+            self.database = database
+            self.transformers = transformers
+
+    def _create_from_namespace_name(
+        self,
+        namespace_name,
+        environment=None,
+        cluster=None,
+        database=None,
+        transformers=[]
+    ):
+        self._validate_namespace_name(namespace_name)
         sections = namespace_name.split('.')
-        if sections[0] in self.ENVIRONMENTS:
+        if self._is_first_section_an_environment(
+            sections,
+            environment=environment,
+            cluster=cluster,
+            database=database,
+            transformers=transformers
+        ):
             cluster_pos = 1
             self.environment = sections[0]
         else:
             cluster_pos = 0
             self.environment = None
         self.cluster = sections[cluster_pos]
-        self.database = sections[cluster_pos+1]
-        self.transformers = sections[cluster_pos+2:]
+        self.database = sections[cluster_pos + 1]
+        self.transformers = sections[cluster_pos + 2:]
 
+    def _is_first_section_an_environment(
+        self,
+        sections,
+        environment=None,
+        cluster=None,
+        database=None,
+        transformers=[]
+    ):
+        """Determines whether or not the first section is an environment or cluster definition"""
+        return (
+            sections[0] in self.ENVIRONMENTS and
+            # ex: main.database
+            len(sections) >= 3 and
+            # ex: main.database.transformers (cluster=main)
+            (cluster is None or sections[1] == cluster) and
+            # ex: main.database.transformers (database=database)
+            (database is None or sections[2] == database) and
+            # ex: main.database.tr1.tr2 (transformers=[tr1])
+            (set(transformers).issubset(set(sections[3:])))
+        ) or sections[0] == environment
+
+    def _validate_namespace_name(self, namespace_name):
+        if re.match("^[a-zA-Z0-9_\.-]+$", namespace_name) is None:
+            raise ValueError("namespace_name must contain only alphanumeric characters and -, _, .")
+        if len(namespace_name.split('.')) < 2:
+            raise ValueError("not enough sections to split namespace_name to extract information")
+
+    def _validate_optional_params_satisfied(
+        self,
+        environment=None,
+        cluster=None,
+        database=None,
+        transformers=[]
+    ):
+        if (
+            (environment is not None and environment != self.environment) or
+            (cluster is not None and cluster != self.cluster) or
+            (database is not None and database != self.database) or
+            not set(transformers).issubset(set(self.transformers))
+        ):
+            raise ValueError("Given namespace_name and optional parameters seem impossible to rectify")
+
+    def get_name(self):
+        output = ""
+        if self.environment is not None:
+            output += self.environment + "."
+        output += self.cluster + "."
+        output += self.database
+        if self.transformers:
+            output += "."
+        output += '.'.join(self.transformers)
+        return output
--- tests/_namespace_util_test.py
+++ tests/_namespace_util_test.py
@@ -4,27 +4,43 @@ from __future__ import unicode_literals
 
 import pytest
 
-from data_pipeline._namespace_util import NamespaceInfo
+from data_pipeline._namespace_util import Namespace
 
-class TestNamespaceInfo(object):
+
+class TestNamespace(object):
     def test_simple(self):
+        name = "refresh_primary.yelp"
         self._verify_success(
-            name="refresh_primary.yelp",
+            namespace=Namespace(name),
+            name=name,
             cluster="refresh_primary",
             database="yelp"
         )
 
-    def test_environment(self):
+    def test_main_cluster(self):
+        name = "main.database"
         self._verify_success(
-            name="main.refresh_primary.yelp",
+            namespace=Namespace(name),
+            name=name,
+            cluster="main",
+            database="database"
+        )
+
+    def test_environment(self):
+        name = "main.refresh_primary.yelp"
+        self._verify_success(
+            namespace=Namespace(name),
+            name=name,
             cluster="refresh_primary",
             database="yelp",
             environment="main"
         )
 
     def test_tranformers(self):
+        name = "dev.refresh_primary.yelp.heartbeat.yelp-main_transformed"
         self._verify_success(
-            name="dev.refresh_primary.yelp.heartbeat.yelp-main_transformed",
+            namespace=Namespace(name),
+            name=name,
             cluster="refresh_primary",
             database="yelp",
             environment="dev",
@@ -32,17 +48,168 @@ class TestNamespaceInfo(object):
         )
 
     def test_fail_missing(self):
-        self._verify_failure("yelp")
-        self._verify_failure("refresh_primary")
+        self._verify_failure("yelp", error_substr="not enough sections")
+        self._verify_failure("refresh_primary", error_substr="not enough sections")
 
-    def _verify_failure(self, name):
-        with pytest.raises(ValueError):
-            NamespaceInfo(name)
+    def test_fail_invalid_chars(self):
+        self._verify_failure("^refresh_primary.yelp", error_substr="must contain only")
+        self._verify_failure("fadjskl;.fjd", error_substr="must contain only")
 
-    def _verify_success(self, name, cluster, database, environment=None, transformers=[]):
-        namespace_info = NamespaceInfo(name)
-        assert namespace_info.cluster == cluster
-        assert namespace_info.database == database
-        assert namespace_info.environment == environment
-        assert namespace_info.transformers == transformers
+    def test_optional_params(self):
+        name = "main.database.transformer"
+        self._verify_success(
+            namespace=Namespace(name, cluster="main"),
+            name=name,
+            cluster="main",
+            database="database",
+            transformers=["transformer"]
+        )
 
+    def test_optional_params_db(self):
+        name = "main.database.transformer"
+        self._verify_success(
+            namespace=Namespace(name, database="database"),
+            name=name,
+            cluster="main",
+            database="database",
+            transformers=["transformer"]
+        )
+
+    def test_optional_params_transformer(self):
+        name = "main.database.transformer"
+        self._verify_success(
+            namespace=Namespace(name, transformers=["transformer"]),
+            name=name,
+            cluster="main",
+            database="database",
+            transformers=["transformer"]
+        )
+
+    def test_double_main(self):
+        name = "main.main.database.transformer"
+        self._verify_success(
+            namespace=Namespace(name, cluster="main"),
+            name=name,
+            environment="main",
+            cluster="main",
+            database="database",
+            transformers=["transformer"]
+        )
+
+    def test_double_main_auto(self):
+        name = "main.main.database.transformer"
+        self._verify_success(
+            namespace=Namespace(name),
+            name=name,
+            environment="main",
+            cluster="main",
+            database="database",
+            transformers=["transformer"]
+        )
+
+    def test_double_main_no_env(self):
+        name = "main.main.transformer"
+        self._verify_success(
+            namespace=Namespace(name, database="main"),
+            name=name,
+            cluster="main",
+            database="main",
+            transformers=["transformer"]
+        )
+
+    def test_double_main_no_env_transformer(self):
+        name = "main.main.transformer"
+        self._verify_success(
+            namespace=Namespace(name, transformers=["transformer"]),
+            name=name,
+            cluster="main",
+            database="main",
+            transformers=["transformer"]
+        )
+
+    def test_fail_impossible(self):
+        name = "dev.refresh_primary.yelp.transformer"
+        self._verify_failure(
+            name,
+            environment="main",
+            error_substr="impossible to rectify"
+        )
+
+    def test_fail_impossible_transformers(self):
+        name = "dev.refresh_primary.yelp.transformer"
+        self._verify_failure(
+            name,
+            transformers=["heartbeat"],
+            error_substr="impossible to rectify"
+        )
+
+    def test_fail_impossible_double_cluster_env(self):
+        name = "dev.refresh_primary.yelp.transformer"
+        self._verify_failure(
+            name,
+            environment="dev",
+            cluster="dev",
+            error_substr="impossible to rectify"
+        )
+
+    def test_fail_impossible_env_db(self):
+        name = "dev.refresh_primary.yelp.transformer"
+        self._verify_failure(
+            name,
+            environment="dev",
+            database="refresh_primary",
+            error_substr="impossible to rectify"
+        )
+
+    def test_no_name(self):
+        self._verify_success(
+            namespace=Namespace(
+                environment="main",
+                cluster="refresh_primary",
+                database="yelp"
+            ),
+            name="main.refresh_primary.yelp",
+            environment="main",
+            cluster="refresh_primary",
+            database="yelp"
+        )
+
+    def test_no_name_no_env(self):
+        self._verify_success(
+            namespace=Namespace(
+                cluster="refresh_primary",
+                database="yelp",
+                transformers=["heartbeat"]
+            ),
+            name="refresh_primary.yelp.heartbeat",
+            cluster="refresh_primary",
+            database="yelp",
+            transformers=["heartbeat"]
+        )
+
+    def _verify_failure(
+        self,
+        name,
+        environment=None,
+        cluster=None,
+        database=None,
+        transformers=[],
+        error_substr=None
+    ):
+        with pytest.raises(ValueError) as e:
+            Namespace(
+                name,
+                environment=environment,
+                cluster=cluster,
+                database=database,
+                transformers=transformers
+            )
+            if error_substr is not None:
+                assert error_substr in e
+
+    def _verify_success(self, namespace, name, cluster, database, environment=None, transformers=[]):
+        assert namespace.get_name() == name
+        assert namespace.cluster == cluster
+        assert namespace.database == database
+        assert namespace.environment == environment
+        assert namespace.transformers == transformers
