625809a9710048351bc32cc733ef14e3bf76ce21
Abrar Sheikh <abrar@yelp.com>
2016-11-01 17:33:37 -0700

    fix linting error

--- data_pipeline/producer.py
+++ data_pipeline/producer.py
@@ -48,9 +48,9 @@ class Producer(Client):
     a number of messages are accumulated, or too much time has passed,
     then published all at once.  This process is designed to be largely
     transparent to the user.
-    
+
     .. note::
-    
+
         The clientlib used to include an AsyncProducer, which published to Kafka in the
         background.  This producer was somewhat flaky, increased development effort,
         and didn't provide a concrete performance benefit (see
